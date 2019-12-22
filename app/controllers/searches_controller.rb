6df9c951fc0ec604b2dd342556b8be18414f9d41
craig <craigya89@gmail.com>
2019-12-22 17:37:53 +0800

    Simple_Search V2 - Current_user session and no login session - if session count > search_condition's count-1 then clean

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
-
   layout 'search'
+  
   def simple
     search_conditions = Animal.search_kind(params[:animal_kind]).search_sex(params[:animal_sex]).search_age(params[:animal_age])
 
@@ -11,19 +11,33 @@ class SearchesController < ApplicationController
 
     @simple_search = data_array.sample(1)
 
-    if session[:a_list] == nil
-      session[:a_list]= []
-    else
-      while session[:a_list].include?(@simple_search[0].id)
+    if user_signed_in?
+
+      current_user.search_session.map! { |x| x.to_i }
+      while current_user.search_session.include?(@simple_search[0].id)
         @simple_search = data_array.sample(1)
-  
-        unless session[:a_list].include?(@simple_search[0].id)
+ 
+        unless current_user.search_session.include?(@simple_search[0].id)
           break
         end
       end
+      current_user.search_session << @simple_search[0].id
+      current_user.search_session = [] if current_user.search_session.count > ((data_array.count) - 1) 
+      current_user.save
+    else
+      if session[:a_list] == nil
+        session[:a_list]= []
+      else
+        while session[:a_list].include?(@simple_search[0].id)
+          @simple_search = data_array.sample(1)
+    
+          unless session[:a_list].include?(@simple_search[0].id)
+            break
+          end
+        end
+      end
+      session[:a_list] << @simple_search[0].id
+      session[:a_list] = [] if session[:a_list].count > ((data_array.count) - 1)
     end
-    session[:a_list] << @simple_search[0].id
-    session[:a_list] = [] if session[:a_list].count > ((data_array.count) - 1)
   end
-
 end
