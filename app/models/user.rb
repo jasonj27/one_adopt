592057402b530893c8d7b6ffd66b33adcf254a9d
簡維德 <aeiou501@gmail.com>
2019-12-23 17:49:16 +0800

    login index page miler complete google login(WIP)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
   has_many :animals
   has_many :favorites
   has_many :reservations
