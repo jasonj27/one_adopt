02907137a2b5dd0f3708f0c27e2e8dc6b3ceaf4c
簡維德 <aeiou501@gmail.com>
2019-12-25 16:56:10 +0800

    sass bug fixed rename pages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
-  root 'page#index'
+  root 'pages#index'
 
   resource :page, only: [:index]  #for landingpage
 
