6a27f91502992fe98a8288b509c6eb52995754ae
Jason Jiang <ssdog27@gmail.com>
2019-12-31 10:33:58 +0800

    修正sender更新送養資料後，is_sender欄位的布林值

--- app/controllers/sender/users_controller.rb
+++ app/controllers/sender/users_controller.rb
@@ -6,19 +6,18 @@ class Sender::UsersController < ApplicationController
   def update
     @user = User.find(current_user.id)
     if @user.update(user_params)
-      if current_user.is_sender == false
-        current_user.is_sender = true
+      if !current_user.is_sender
+        current_user.update(is_sender: true)
       end
-      redirect_to sender_root_path, notice: '更新成功'
+      redirect_to sender_root_path, notice: "更新成功"
     else
       render :edit
     end
-
   end
 
   private
-  def user_params
-    params.require(:user).permit(:sender_tel, :sender_add, :days,:w0, :w1, :w2, :w3, :w4, :w5, :w6)
-  end
 
+  def user_params
+    params.require(:user).permit(:sender_tel, :sender_add, :days, :w0, :w1, :w2, :w3, :w4, :w5, :w6)
+  end
 end
