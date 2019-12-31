da59711e653d15dbd921707cc17a6e32f2c00f33
Jason Jiang <ssdog27@gmail.com>
2019-12-31 10:56:28 +0800

    移除一個多的end(favorites_controller)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,31 +4,30 @@ class ReservationsController < ApplicationController
       redirect_to user_session_path
     end
 
-    @favorites = current_user.favorites.includes({animal: :user}, :animal)
-    @sort_fav={}
+    @favorites = current_user.favorites.includes({ animal: :user }, :animal)
+    @sort_fav = {}
     @favorites.each do |fav|
       if @sort_fav.has_key?(fav.animal.user.name)
-        @sort_fav[fav.animal.user.name]['animals'].push([fav.animal.name, fav.animal.id])
+        @sort_fav[fav.animal.user.name]["animals"].push([fav.animal.name, fav.animal.id])
       else
-        @sort_fav.store(fav.animal.user.name , {'id'=> fav.animal.user.id, 'animals'=> [[fav.animal.name, fav.animal.id]]})
+        @sort_fav.store(fav.animal.user.name, { "id" => fav.animal.user.id, "animals" => [[fav.animal.name, fav.animal.id]] })
       end
     end
   end
 
   def index
-    @reservations = current_user.receiver_reservations.includes(:sender, {reservation_pets: :animal})
+    @reservations = current_user.receiver_reservations.includes(:sender, { reservation_pets: :animal })
   end
 
-
   def new
-    @reservation = Reservation.new(sender_id: params['sender'], receiver_id: current_user.id)
+    @reservation = Reservation.new(sender_id: params["sender"], receiver_id: current_user.id)
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
     p reservation_params
     if @reservation.save
-      @favorites = current_user.favorites.includes({animal: :user}, :animal)
+      @favorites = current_user.favorites.includes({ animal: :user }, :animal)
       @favorites.each do |fav|
         if fav.animal.user.id == @reservation.sender_id
           ReservationPet.create(animal_id: fav.animal.id, reservation_id: @reservation.id)
@@ -49,5 +48,4 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation).permit(:note, :date, :time, :sender_id).merge("datetime" => ("#{params["reservation"]["date"]} #{params["reservation"]["time"]}"), "receiver_id" => current_user.id).except(:date, :time)
   end
-
 end
