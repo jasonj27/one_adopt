7c97f249c60f9c12e8f1da00363f3b8f9915678c
Jason Jiang <ssdog27@gmail.com>
2020-01-12 16:39:37 +0800

    action text style

--- app/javascript/packs/trix-overrides.js
+++ app/javascript/packs/trix-overrides.js
@@ -1,12 +1,12 @@
 window.addEventListener("trix-file-accept", function(event) {
-  const acceptedTypes = ['image/jpeg', 'image/png']
+  const acceptedTypes = ['image/jpeg', 'image/png', 'application/pdf']
   if (!acceptedTypes.includes(event.file.type)) {
     event.preventDefault()
-    alert("只能附加JPEG或PNG圖片！")
+    alert("只能附加PDF文件或JPEG和PNG圖片！")
   }
-  const maxFileSize = 2048 * 1024 //2MB
+  const maxFileSize = 2 * 1024 * 1024 //2MB
   if (event.file.size > maxFileSize) {
     event.preventDefault()
-    alert("單張圖片最大檔案大小為2MB!")
+    alert("單一檔案最大檔案大小為2MB!")
   }
 })
