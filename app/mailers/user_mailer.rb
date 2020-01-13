a90141c29c5c4424ab109ecb77ee8684496691b3
簡維德 <aeiou501@gmail.com>
2020-01-13 13:53:36 +0800

    change mailer from gmail to mailgun, change reset password email content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
     headers["Custom-header"] = "Bar"
     opts[:subject] = '貓狗大平台帳號確認信'
-    opts[:from] = 'one_adopt'
-    opts[:reply_to] = 'one_adopt'
+    opts[:from] = 'One_adopt <postmaster@mg.oneadopt.today>'
+    opts[:reply_to] = 'One_adopt'
     super
   end
 end
