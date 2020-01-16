6b21a0e53912c89259155fa827baa32e1a71f12b
craig <craigya89@gmail.com>
2020-01-16 22:13:52 +0800

    gem Active Record Session Store

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module DemoProject
       config.active_storage.variant_processor = :vips
     end
 
+    ActiveRecord::SessionStore::Session.table_name = 'sessions'
+    ActiveRecord::SessionStore::Session.primary_key = 'session_id'
+    ActiveRecord::SessionStore::Session.data_column_name = 'data'
+    ActiveRecord::SessionStore::Session.serializer = :json
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_082735) do
+ActiveRecord::Schema.define(version: 2020_01_16_140441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "cube"
@@ -150,6 +150,15 @@ ActiveRecord::Schema.define(version: 2020_01_15_082735) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["receiver_id"], name: "index_reservations_on_receiver_id"
     t.index ["sender_id"], name: "index_reservations_on_sender_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
