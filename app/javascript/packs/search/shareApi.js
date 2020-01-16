ce9b65ef85cc044194165f002489faa93a8e1129
craig <craigya89@gmail.com>
2020-01-16 13:15:31 +0800

    - Fix safari不能分享與複製 - 將未登入, 按收藏後路徑轉至root_path

--- app/javascript/packs/search/shareApi.js
+++ app/javascript/packs/search/shareApi.js
@@ -1,66 +1,61 @@
 document.addEventListener("DOMContentLoaded", function(evt) {
-  $('document').ready(function(){
     document.querySelectorAll('.share-button').forEach(item => {
       item.addEventListener('click', event => {
         let shareAnimalID = item.dataset.share
-        
-        $.get(`https://www.oneadopt.today/searches/${shareAnimalID}.json`, function(data){  
-          let dataAnimalID = data.id
-          const shareBtn = document.querySelector('.share-button');
-          const shareResult = document.querySelector('.share-result');
-          const shareClose = document.querySelector(".share-close");
-          const shareData = {
-            url: `https://www.oneadopt.today/searches/${dataAnimalID}`,
-            title: '趕快來看看吧!',
-            text: '毛孩需要讓更多人看見',
-          };
-            
-          if (navigator.share) {
-            handleNavigatorShare();
-          } else {
-            handleNotSupportNavigatorShare();
-          }
-        
-          async function handleNavigatorShare() {
-            try {
-              await navigator.share(shareData);
+          
+        const shareBtn = document.querySelector('.share-button');
+        const shareResult = document.querySelector('.share-result');
+        const shareClose = document.querySelector(".share-close");
+        const shareData = {
+          url: `https://oneadopt.today/searches/${shareAnimalID}`,
+          title: '趕快來看看吧!',
+          text: '毛孩需要讓更多人看見',
+        };
+          
+        if (navigator.share) {
+          handleNavigatorShare();
+        } else {
+          handleNotSupportNavigatorShare();
+        }
+      
+        function handleNavigatorShare() {
+          try {
+            navigator.share(shareData);
+            shareResult.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show");
+            shareResult.setAttribute("role", "alert");
+            shareResult.append('感謝您的分享!');
+            shareClose.classList.remove("d-none");
+          } catch (err) {
+            const { name } = err;
+            if (name === 'AbortError') {
               shareResult.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show");
               shareResult.setAttribute("role", "alert");
-              shareResult.append('感謝您的分享!');
+              shareResult.append('您已取消分享此訊息!');
               shareClose.classList.remove("d-none");
-            } catch (err) {
-              const { name } = err;
-              if (name === 'AbortError') {
-                shareResult.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show");
-                shareResult.setAttribute("role", "alert");
-                shareResult.append('您已取消分享此訊息!');
-                shareClose.classList.remove("d-none");
-              } else {
-                shareResult.textContent = err;
-                alert('發生錯誤', err);
-              }
+            } else {
+              shareResult.textContent = err;
+              alert('發生錯誤', err);
             }
           }
-        
-          function handleNotSupportNavigatorShare() {
-            const contentToCopy = document.querySelector('#content-to-copy');
-            contentToCopy.value = shareData.url;
-            contentToCopy.setAttribute('type', 'text');
-            contentToCopy.select();
-        
-            try {
-              const successful = document.execCommand('copy');
-              const msg = successful ? '成功' : '失敗';
-              alert(`連結複製${msg}，快去分享吧！`);
-            } catch (err) {
-              alert('發生錯誤，無法複製連結');
-            }
-        
-            contentToCopy.setAttribute('type', 'hidden');
-            window.getSelection().removeAllRanges();
+        }
+      
+        function handleNotSupportNavigatorShare() {
+          const contentToCopy = document.querySelector('#content-to-copy');
+          contentToCopy.value = shareData.url;
+          contentToCopy.setAttribute('type', 'text');
+          contentToCopy.select();
+      
+          try {
+            const successful = document.execCommand('copy');
+            const msg = successful ? '成功' : '失敗';
+            alert(`連結複製${msg}`);
+          } catch (err) {
+            alert('發生錯誤，無法複製連結');
           }
-        })
-      })
+      
+          contentToCopy.setAttribute('type', 'hidden');
+          window.getSelection().removeAllRanges();
+        }
     })
   })
 })
