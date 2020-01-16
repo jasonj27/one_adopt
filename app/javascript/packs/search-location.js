67705db9142460526d46d6ff4f1cf14152e41392
Jason Jiang <ssdog27@gmail.com>
2020-01-17 01:25:59 +0800

    using Maps JavaScript API to show location

--- app/javascript/packs/search-location.js
+++ app/javascript/packs/search-location.js
@@ -1,31 +1,58 @@
 import $ from "jquery"
 import 'bootstrap'
+import loadGoogleMapsApi from 'load-google-maps-api/index'
 
 document.addEventListener("DOMContentLoaded", function() {
-  function getImg(){
-    let lat = $('#latitude').val()
-    let lng = $('#longitude').val()
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=18&size=400x250&scale=2&maptype=roadmap&markers=color:red|${lat},${lng}&key=AIzaSyDkzbhgrcZprbdEP9aU0GqRTuKdG9r2zAg`
-  }
-  if ("geolocation" in navigator){
-    $('#distance').on("change", function(){
-      let watchID = navigator.geolocation.watchPosition(function(position) {
-        console.log(position.coords.latitude, position.coords.longitude)
-        $('#latitude').val(position.coords.latitude)
-        $('#longitude').val(position.coords.longitude)
-        $('#locationButton').attr("disabled", false)
-        let imgUrl = getImg()
-        let insertBody = `<div class=""><div>您的位置：</div><img src=\"${imgUrl}\" style="width: 100%;"></div>`
-        $('.modal-body').html(insertBody)
+  loadGoogleMapsApi({key: 'AIzaSyDkzbhgrcZprbdEP9aU0GqRTuKdG9r2zAg'}).then(function(googleMaps){
+    let map
+    let infoWindow
+
+    map = new googleMaps.Map(document.getElementById('map'), {
+      center: {
+        lat: 25.0424356, 
+        lng: 121.5137072
       },
-      ()=>{},
-      {enableHighAccuracy: true, timeout: 10000})
-      if ($('#latitude').val() === "" || $('#longitude').val() === ""){
-        $('#locationButton').attr("disabled", true)
-        $('#locationModal').modal('show')
+        zoom: 17
+      })
+
+    infoWindow = new googleMaps.InfoWindow
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            '定位失敗' :
+                            '您的瀏覽器不支援定位服務')
+      infoWindow.open(map)
+      $('#locationButton').attr("disabled", false)
+    }
+    if ("geolocation" in navigator){
+      $('#distance').on("change", function(){
+        navigator.geolocation.watchPosition(function(position) {
+          console.log(position.coords.latitude, position.coords.longitude)
+          let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+          infoWindow.setPosition(pos)
+          infoWindow.setContent('您的位置')
+          infoWindow.open(map)
+          map.setCenter(pos)
+          $('#latitude').val(pos.lat)
+          $('#longitude').val(pos.lng)
+          $('#locationButton').attr("disabled", false)
+        },
+        ()=>{handleLocationError(true, infoWindow, map.getCenter())},
+        {enableHighAccuracy: true, timeout: 10000})
+        if ($('#latitude').val() === "" || $('#longitude').val() === ""){
+          $('#locationButton').attr("disabled", true)
+          $('#locationModal').modal('show')
+        }
+      })
+      }else{
+        handleLocationError(false, infoWindow, map.getCenter())
       }
+
+    }).catch(function (error) {
+      console.error(error)
     })
-  }else{
-    alert("無法取得位置")
-  }
 })
